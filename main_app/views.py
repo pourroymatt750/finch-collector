f9c80cc5338ee92f47767f722216d463bc47a646
Matthew Pourroy <pourroy.matt@gmail.com>
2022-08-05 00:23:10 -0400

    testing

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DeleteView
+from .models import Finch
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello World</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
