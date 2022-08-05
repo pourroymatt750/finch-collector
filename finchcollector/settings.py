3d604e9249a38903d2223bc36f157318798a716c
Matthew Pourroy <pourroy.matt@gmail.com>
2022-08-05 00:32:14 -0400

    done part 3

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mao!=t5_2&ls=z2*lu_l9g38999+frvumg8=#j%41_rduumw%y'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
